9a24379986148ae59b9012371fa11343f732e1e2
rog2014 <rogerghv@gmail.com>
2022-06-07 13:39:35 -0600

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,33 +1,33 @@
-# Copyright (C) 2010 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+
 {
-    "name": "Hazard",
-    "version": "14.0.1.0.1",
-    "author": "Savoir-faire Linux, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/management-system",
+    "name": "Document Page",
+    "version": "14.0.1.0.0",
+    "category": "Knowledge Management",
+    "author": "OpenERP SA, Odoo Community Association (OCA)",
+    "images": [
+        "images/category_list.png",
+        "images/create_category.png",
+        "images/page_list.png",
+        "images/create_page.png",
+        "images/customer_invoice.jpeg",
+        "images/page_history.png",
+    ],
+    "website": "https://github.com/OCA/knowledge",
     "license": "AGPL-3",
-    "category": "Management System",
-    "depends": ["mgmtsystem", "hr"],
+    "depends": ["mail", "knowledge"],
     "data": [
+        "security/document_page_security.xml",
         "security/ir.model.access.csv",
-        "security/mgmtsystem_hazard_security.xml",
-        "views/mgmtsystem_hazard.xml",
-        "views/mgmtsystem_hazard_origin.xml",
-        "views/mgmtsystem_hazard_type.xml",
-        "views/mgmtsystem_hazard_probability.xml",
-        "views/mgmtsystem_hazard_severity.xml",
-        "views/mgmtsystem_hazard_usage.xml",
-        "views/mgmtsystem_hazard_control_measure.xml",
-        "views/mgmtsystem_hazard_test.xml",
+        "wizard/document_page_create_menu.xml",
+        "wizard/document_page_show_diff.xml",
+        "views/document_page.xml",
+        "views/document_page_category.xml",
+        "views/document_page_history.xml",
+        "views/document_page_assets.xml",
+        "views/report_document_page.xml",
     ],
-    "demo": [
-        "demo/mgmtsystem_hazard_hazard.xml",
-        "demo/mgmtsystem_hazard_origin.xml",
-        "demo/mgmtsystem_hazard_probability.xml",
-        "demo/mgmtsystem_hazard_severity.xml",
-        "demo/mgmtsystem_hazard_type.xml",
-        "demo/mgmtsystem_hazard_usage.xml",
-    ],
-    "installable": True,
+    "demo": ["demo/document_page.xml"],
 }
